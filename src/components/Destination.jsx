c605320c21642ba4a4ccccf7cd4e47a26d1bf825
Rupayan13 <send2rupayan2002@gmail.com>
2025-09-21 12:28:15 +0530

    Adding frontend

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -4,7 +4,7 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "../APP.css";
+import "../App.css";
 import { Typewriter } from 'react-simple-typewriter'
 import { Link } from "react-router-dom";
 
