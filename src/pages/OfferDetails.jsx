c222f2a361f1dbeec52000632f40507264a74e7a
Rupayan13 <send2rupayan2002@gmail.com>
2025-11-23 12:15:15 +0530

    sorted offers by a order

--- src/pages/OfferDetails.jsx
+++ src/pages/OfferDetails.jsx
@@ -4,39 +4,40 @@ import "../App.css";
 
 const offerDetails = {
     1: {
-        title: "Slikroute Tour Package",
+        title: "Darjeeling Package Tour",
         img: "offer/offer1.jpg",
-        description: "4 Nights & 5 Days premium tour package with full sightseeing and stay included.",
-        price: "₹5999",
+        description: "2 Nights, 3 Days beautiful mountain tour across Darjeeling.",
+        price: "₹3999",
         destinations: {
-            day1: "Sillery Gaon or Icche Gaon",
-            day2: "Lingtam or Padamchen",
-            day3: "Zuluk",
-            day4: "Rishi Khola",
-            day5: "Drop to NJP"
+            day1: "NJP to Darjeeling",
+            day2: "Darjeeling local sightseeing",
+            day3: "Darjeeling to NJP Drop"
         }
     },
     2: {
-        title: "Pelling",
+        title: "Darjeeling and Kalimpong Tour",
         img: "offer/offer2.jpg",
-        description: "3 Nights & 4 Days Pelling special package with sightseeing and pickup/drop.",
-        price: "₹5499",
+        description: "4 Nights, 5 Days tour package covering Darjeeling and Kalimpong.",
+        price: "₹6499",
         destinations: {
-            day1: "NJP to Pelling via on Road Sightseeing (Stay at Pelling)",
-            day2: "Pelling sightseeing via kanchanjunga water falls, Khecheopalri Lake, Rimbi water falls, Orange Garden, Sky Walk, Pamayangste Monastery, Stay at Pelling",
-            day3: "Pelling to Ravangla via Rabdentss Ruins, Ravangla Buddha Park, Stay at Ravangla",
-            day4: "Ravangla to NJP via Sightseeing Namchi 4 dham",
+            day1: "Rishop",
+            day2: "Ramadhura",
+            day3: "Takdah",
+            day4: "Darjeeling",
+            day5: "NJP Drop"
         }
     },
     3: {
-        title: "Explore North Sikkim",
+        title: "Darjeeling Offbeat Tour",
         img: "offer/offer3.jpg",
-        description: "4 Nights & 5 Days package covering Gurudongmar, Lachung, Lachen and more.",
-        price: "₹7599",
+        description: "4 Nights, 5 Days tour package covering Darjeeling offbeats.",
+        price: "₹6499",
         destinations: {
-            Gangtok: "2 Night",
-            Lachen: "1 Night",
-            Lachung: "1 Night",
+            day1: "Sittong",
+            day2: "Tinchule",
+            day3: "Darjeeling",
+            day4: "Lepcha Jagat",
+            day5: "NJP Drop via Mirik"
         }
     },
     4: {
@@ -66,42 +67,41 @@ const offerDetails = {
         }
     },
     6: {
-        title: "Darjeeling Package Tour",
+        title: "Explore North Sikkim",
         img: "offer/offer6.jpg",
-        description: "2 Nights, 3 Days beautiful mountain tour across Darjeeling.",
-        price: "₹3999",
+        description: "4 Nights & 5 Days package covering Gurudongmar, Lachung, Lachen and more.",
+        price: "₹7599",
         destinations: {
-            day1: "NJP to Darjeeling",
-            day2: "Darjeeling local sightseeing",
-            day3: "Darjeeling to NJP Drop"
+            Gangtok: "2 Night",
+            Lachen: "1 Night",
+            Lachung: "1 Night",
         }
     },
     7: {
-        title: "Darjeeling and Kalimpong Tour",
+        title: "Slikroute Tour Package",
         img: "offer/offer7.jpg",
-        description: "4 Nights, 5 Days tour package covering Darjeeling and Kalimpong.",
-        price: "₹6499",
+        description: "4 Nights & 5 Days premium tour package with full sightseeing and stay included.",
+        price: "₹5999",
         destinations: {
-            day1: "Rishop",
-            day2: "Ramadhura",
-            day3: "Takdah",
-            day4: "Darjeeling",
-            day5: "NJP Drop"
+            day1: "Sillery Gaon or Icche Gaon",
+            day2: "Lingtam or Padamchen",
+            day3: "Zuluk",
+            day4: "Rishi Khola",
+            day5: "Drop to NJP"
         }
     },
     8: {
-        title: "Darjeeling Offbeat Tour",
+        title: "Pelling",
         img: "offer/offer8.jpg",
-        description: "4 Nights, 5 Days tour package covering Darjeeling offbeats.",
-        price: "₹6499",
+        description: "3 Nights & 4 Days Pelling special package with sightseeing and pickup/drop.",
+        price: "₹5499",
         destinations: {
-            day1: "Sittong",
-            day2: "Tinchule",
-            day3: "Darjeeling",
-            day4: "Lepcha Jagat",
-            day5: "NJP Drop via Mirik"
+            day1: "NJP to Pelling via on Road Sightseeing (Stay at Pelling)",
+            day2: "Pelling sightseeing via kanchanjunga water falls, Khecheopalri Lake, Rimbi water falls, Orange Garden, Sky Walk, Pamayangste Monastery, Stay at Pelling",
+            day3: "Pelling to Ravangla via Rabdentss Ruins, Ravangla Buddha Park, Stay at Ravangla",
+            day4: "Ravangla to NJP via Sightseeing Namchi 4 dham"
         }
-    },
+    }
 };
 
 
